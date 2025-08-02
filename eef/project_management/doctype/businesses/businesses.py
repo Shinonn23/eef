d3fbe2b161accfe487fdf07babf6d98fbfb24f67
Shinonn <siwat61719@gmail.com>
2025-08-03 01:41:08 +0700

    feat: Add project management features including project link in businesses and dashboard data retrieval

--- eef/project_management/doctype/businesses/businesses.py
+++ eef/project_management/doctype/businesses/businesses.py
@@ -6,5 +6,20 @@ from frappe.model.document import Document
 
 
 class Businesses(Document):
+	# begin: auto-generated types
+	# This code is auto-generated. Do not modify anything in this block.
+
+	from typing import TYPE_CHECKING
+
+	if TYPE_CHECKING:
+		from frappe.types import DF
+
+		district: DF.Literal[None]
+		name1: DF.Data | None
+		postal_code: DF.Data | None
+		project: DF.Link | None
+		province: DF.Literal[None]
+		subdistrict: DF.Literal[None]
+	# end: auto-generated types
     
 	pass
