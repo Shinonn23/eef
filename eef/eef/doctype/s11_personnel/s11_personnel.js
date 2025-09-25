03ada3a7521043bfc032dabccc43841c80022174
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2025-09-25 08:27:31 +0700

    Add S11 Personnel doctype with initial fields and integration tests

---
+++ eef/eef/doctype/s11_personnel/s11_personnel.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Siwat Sroisuwan and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("S11 Personnel", {
+// 	refresh(frm) {
+
+// 	},
+// });
