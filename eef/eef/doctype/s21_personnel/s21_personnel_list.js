cd7e8b4bf8df745f82448712c433569dad36281b
Shinonn23 <siwat61719@gmail.com>
2025-11-23 01:26:00 +0700

    feat: add workspace sidebar configuration for educational fund
    - Created a new JSON file for the workspace sidebar of the educational fund, including various sections and links relevant to the fund's operations.
    - Added multiple sidebar items with distinct labels, icons, and link types to enhance user navigation and accessibility.
    - Introduced a yarn.lock file to manage package dependencies and ensure consistent installations across environments.

---
+++ eef/eef/doctype/s21_personnel/s21_personnel_list.js
@@ -0,0 +1,5 @@
+frappe.listview_settings["S21 Personnel"] = {
+    onload: function (listview) {
+        listview.page.set_title(__("ด้านที่ 2.1 การพัฒนาหลักสูตรและกระบวนการเรียนการสอน (อาจารย์ผู้รับผิดชอบหลักสูตร)"));
+    },
+};
