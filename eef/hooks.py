eee4498b050fafc10d4f4b50caccaa12462f1cb1
Shinonn23 <siwat61719@gmail.com>
2026-01-18 11:27:13 +0700

    hot-fix load page error

--- eef/hooks.py
+++ eef/hooks.py
@@ -11,15 +11,15 @@ app_license = "mit"
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-#     {
-#         "name": "eef",
-#         "logo": "/assets/eef/eef-logo.png",
-#         "title": "EEF",
-#         "route": "/app/team",
-#         # "has_permission": "eef.api.permission.has_app_permission"
-#     }
-# ]
+add_to_apps_screen = [
+    {
+        "name": "eef",
+        "logo": "/assets/eef/eef-logo.png",
+        "title": "EEF",
+        "route": "/app/desk",
+        # "has_permission": "eef.api.permission.has_app_permission"
+    }
+]
 
 # Includes in <head>
 # ------------------
